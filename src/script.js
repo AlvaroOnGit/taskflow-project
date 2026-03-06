d85b55a1c408e6267d42cfe1900913fbb8065740
Álvaro Gómez <alvaro.ongit@outlook.com>
2026-03-06 14:18:05 +0100

    Added the functionality to filter activities Added functionality to set multiple tags per activity Added functionality to delete activities Added functionality to edit activity description on runtime Added functionality to change icons on context

--- src/script.js
+++ src/script.js
@@ -1,49 +1,70 @@
+//Header
+const themeIcon = document.getElementById('theme-toggle-icon')
 //Sidebar
 const sidebar = document.getElementById('sidebar');
 const sidebarMobileToggle = document.getElementById('sidebar-mobile-toggle');
 //Tags
-const sidebarTagMenu = document.querySelector('.sidebar-tag-menu');
+const sidebarTagMenu = document.getElementById('sidebar-tag-menu');
 const sidebarTagMenuItemContainer = sidebarTagMenu.parentElement;
 const sidebarTagMenuCollapsibleContent = document.getElementById('tag-menu-collapsible');
 const sidebarTagMenuIcon = document.getElementById('tag-menu-icon');
-const tagForm = document.querySelector(".tag-menu-form");
-const tagFormText = document.getElementById("new-tag-input");
-const tagFormColor = document.getElementById("tag-color-input");
+const tagForm = document.getElementById('tag-menu-form');
+const tagFormText = document.getElementById('new-tag-input');
+const tagFormColor = document.getElementById('tag-color-input');
 const tagList = document.getElementById("tag-menu-list")
 //Composer
 const tagSelector = document.getElementById('activity-tag-selector');
 const tagSelectorIcon = document.getElementById('activity-tag-selector-icon');
 const tagContainer = document.getElementById('activity-tag-container');
 const tagOptions = document.getElementById('activity-tag-options');
-
+const activityComposerForm = document.getElementById('activity-form');
+const activityComposerInput = document.getElementById('activity-composer-input');
+//Activities
+const activityContainer = document.getElementById('activity-container');
+//Filters
+const activitySearch = document.getElementById('activity-filter-search');
 
 let tags = JSON.parse(localStorage.getItem('tags')) || [];
 let activities = JSON.parse(localStorage.getItem('activities')) || [];
 
 renderUserTags();
+renderUserActivities();
 
 /**
  * Handles the button to enable and disable darkmode
  */
-document.addEventListener("DOMContentLoaded", event => {
+document.addEventListener("DOMContentLoaded", () => {
+
     const html = document.documentElement;
-    const themeButton = document.getElementById("button-toggle-theme");
 
-    themeButton.addEventListener('click', () => {
-        const isDark = html.classList.toggle('dark');
+    const savedTheme = localStorage.getItem('theme');
+    let isDark = savedTheme === 'dark';
 
-        localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    });
-
-    if (localStorage.getItem('theme') === 'dark') {
+    if (isDark) {
         html.classList.add('dark');
     }
+
+    const updateButtonUI = (dark) => {
+        themeIcon.textContent = dark ? 'light_mode' : 'dark_mode';
+    };
+
+    updateButtonUI(isDark);
+
+    themeIcon.addEventListener('click', () => {
+
+        isDark = html.classList.toggle('dark');
+
+        localStorage.setItem('theme', isDark ? 'dark' : 'light');
+
+        updateButtonUI(isDark);
+    });
 });
 
 /**
  * Handles the functionality for rendering the sidebar on mobile
  */
 sidebarMobileToggle.addEventListener('click', () => {
+
     sidebar.classList.toggle('is-open');
 });
 
@@ -52,6 +73,7 @@ sidebarMobileToggle.addEventListener('click', () => {
  * Handles the functionality for rendering the users tags on the sidebar
  */
 function renderUserTags() {
+
     tagList.innerHTML = '';
 
     tags.forEach((tag, index) => {
@@ -64,7 +86,7 @@ function renderUserTags() {
                 ${tag.name}
             </span>
             <button class="tag-menu-button bg-activity-delete p-0.5 rounded text-xs" 
-                    onclick="deleteUserTag(${index})">x</button>
+                    onclick="deleteUserTag(${index})">X</button>
         `;
         tagList.appendChild(li);
     });
@@ -75,6 +97,7 @@ function renderUserTags() {
  * It also calls for an update on the tag selector on the activity composer
  */
 function saveAndRenderTags() {
+
     localStorage.setItem('tags', JSON.stringify(tags));
     renderUserTags();
     updateTagSelector();
@@ -109,6 +132,7 @@ tagForm.addEventListener("submit", (e) => {
  * Calls for rendering to update the tag list
  */
 window.deleteUserTag = (index) => {
+
     tags.splice(index, 1);
     saveAndRenderTags();
 };
@@ -118,7 +142,9 @@ window.deleteUserTag = (index) => {
  * It also changes the arrow icon depending on context
  */
 sidebarTagMenu.addEventListener('click', () => {
+
     sidebarTagMenuItemContainer.classList.toggle('is-active');
+
     if (sidebarTagMenuIcon.textContent === 'keyboard_arrow_down') {
         sidebarTagMenuIcon.textContent = 'keyboard_arrow_up';
     }
@@ -139,7 +165,9 @@ let selectedTagsForNewActivity = [];
  * It also changes the arrow icon depending on context
  */
 tagSelector.onclick = (e) => {
+
     e.stopPropagation();
+
     tagOptions.classList.toggle('hidden');
     if (tagSelectorIcon.textContent === 'keyboard_arrow_down') {
         tagSelectorIcon.textContent = 'keyboard_arrow_up';
@@ -149,12 +177,14 @@ tagSelector.onclick = (e) => {
     }
 };
 
+/**
+ * Handles the arrow icon for the tag selector on the activity composer
+ */
 document.addEventListener('click', () => {
+
     tagOptions.classList.add('hidden');
-    if (tagSelectorIcon.textContent === 'keyboard_arrow_down') {
-        tagSelectorIcon.textContent = 'keyboard_arrow_up';
-    }
-    else {
+
+    if (tagSelectorIcon.textContent === 'keyboard_arrow_up') {
         tagSelectorIcon.textContent = 'keyboard_arrow_down';
     }
 });
@@ -195,11 +225,12 @@ function updateTagSelector() {
  * Allows the user to delete selected tags as well
  */
 function renderComposerTags() {
+
     tagContainer.innerHTML = '';
     selectedTagsForNewActivity.forEach((tag, index) => {
         const span = document.createElement('span');
 
-        span.className = 'activity-tag cursor-pointer hover:opacity-80 transition-opacity border p-1 rounded-lg text-xs font-bold';
+        span.className = 'activity-tag cursor-pointer hover:!bg-red-500/50 hover:!border-red-500 hover:!text-red-500 transition-opacity border p-0.5 rounded-lg text-xs font-bold';
 
         span.style.color = tag.color;
         span.style.borderColor = tag.color;
@@ -221,6 +252,7 @@ function renderComposerTags() {
  * Updates the tag selector afterward
  */
 function addTagToSelection(tagName, tagColor) {
+
     if (tagName && !selectedTagsForNewActivity.some(t => t.name === tagName)) {
         selectedTagsForNewActivity.push({ name: tagName, color: tagColor });
         renderComposerTags();
@@ -229,3 +261,138 @@ function addTagToSelection(tagName, tagColor) {
 }
 
 updateTagSelector();
+
+
+//Activities
+/**
+ * Handles the functionality for rendering the users activities
+ */
+function renderUserActivities(filter = "") {
+
+    activityContainer.innerHTML = '';
+
+    activities.forEach((activity, index) => {
+
+        const li = document.createElement('li');
+
+        const activityTags = activity.tags.map(tag => `
+            <span class="activity-tag" 
+                  style="border-color: ${tag.color}; color: ${tag.color}; background-color: ${tag.color}20">
+                ${tag.name}
+            </span>
+        `).join('');
+
+        li.innerHTML = `
+            <article class="activity-item">
+                <div class="activity-data">
+                    <div class="activity-info">
+                        <h3>${activity.name}</h3>
+                        <div class="activity-tags">
+                            ${activityTags}
+                        </div>
+                    </div>
+                    <textarea 
+                        class="activity-description-input"
+                        placeholder="Descripción de la actividad...."
+                        oninput="autoResize(this)"
+                        onblur="updateActivityDescription(${index}, this.value)"
+                    >${activity.description || ''}</textarea>
+                    <div class="activity-delete-background"></div>
+                        <button class="activity-delete-button" onclick="deleteUserActivity(${index})">X</button>
+                    </div>
+                </div>
+            </article>
+        `;
+
+        activityContainer.appendChild(li);
+    });
+}
+
+/**
+ * Handles the resizing of the textarea to control overflow on runtime
+ */
+function autoResize(textarea) {
+
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+}
+
+/**
+ * Handles the functionality for saving user activities to localStorage
+ * Calls for the rendering of activities afterward
+ */
+function saveAndRenderActivities() {
+
+    localStorage.setItem('activities', JSON.stringify(activities));
+    renderUserActivities();
+}
+
+/**
+ * Handles the creation of activities
+ * Clears the tag container on the composer after creation
+ */
+activityComposerForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const activityName = activityComposerInput.value;
+
+    if (activityName === "" || activities.some(a => a.name === activityName)) return;
+
+    const newActivity = {
+        name: activityName,
+        tags: [...selectedTagsForNewActivity],
+        description: '',
+    }
+
+    activities.push(newActivity);
+
+    selectedTagsForNewActivity.length = 0
+    activityComposerInput.value = "";
+    renderComposerTags();
+    updateTagSelector();
+    saveAndRenderActivities();
+})
+
+/**
+ * Updates the description value of the activity on localStorage
+ * Calls for the rendering of activities afterward
+ */
+function updateActivityDescription(index, newValue) {
+
+    activities[index].description = newValue;
+    saveAndRenderActivities();
+}
+
+/**
+ * Handles the deletion of activities
+ * Calls for the rendering of activities afterward
+ */
+window.deleteUserActivity = (index) => {
+
+    activities.splice(index, 1);
+    saveAndRenderActivities();
+};
+
+/**
+ * Handles the filtering of activities
+ */
+
+activitySearch.addEventListener('input', () => {
+
+    const activityList = document.querySelectorAll('.activity-container li');
+
+    const searchTerm = activitySearch.value.toLowerCase();
+
+    activityList.forEach(li => {
+
+        const titleElement = li.querySelector('.activity-info h3');
+
+        if (titleElement) {
+
+            const title = titleElement.textContent.toLowerCase();
+
+            li.style.display = title.includes(searchTerm) ? 'block' : 'none';
+        }
+    })
+});
